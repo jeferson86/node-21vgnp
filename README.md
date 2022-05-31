fd79a355125da8d3c48c873a069e6aa48c0ef333
jeferson86 <jeferson86silva@gmail.com>
2022-05-31 16:47:18 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-21vgnp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-21vgnp)
---
+++ index.js
@@ -0,0 +1,16 @@
+// run `node index.js` in the terminal
+
+console.log(`Hello Node.js v${process.versions.node}!`);
+const somosCasados = true
+const anoNascimento = 1980
+const media = (7+8+9+10)/4
+const nome = 'Vanessa'
+let idade = null
+const terceiroFilho
+const numeroGrande = 1233434456456456456n
+const numeroGrande = BigInt('223423432423423n')
+const meuSimbolo = new Synmbol()
+const meuObjeto = {
+  curso: 'Estrutura de Dados',
+  instrutores: ['Gabriel','Vanessa']
+}
